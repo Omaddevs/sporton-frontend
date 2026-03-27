844282eb8452a6909ef9a8ed59ea6f272b17dee6
Omadbek Egamberdiyev <omaddevs@gmail.com>
2026-03-27 15:16:52 +0500

    logo updates

--- src/pages/Profile/AuthPage.jsx
+++ src/pages/Profile/AuthPage.jsx
@@ -66,7 +66,7 @@ export default function AuthPage({ onLogin }) {
         <div className="auth-hero-icon">
           <img src={logoImg} alt="SportON" style={{ height: '42px', width: 'auto', objectFit: 'contain', display: 'block' }} />
         </div>
-        <h1 className="auth-hero-title">SportON</h1>
+        {/* <h1 className="auth-hero-title"></h1> */}
         <p className="auth-hero-sub">O'zbekistondagi sport zallari</p>
       </div>
 
